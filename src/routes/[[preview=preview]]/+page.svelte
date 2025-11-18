4daae7414756077d04e55afd8a6537e43da6d0f4
Tucker Lemos <43099664+tucksravin@users.noreply.github.com>
2025-11-18 15:32:41 -0800

    mobile

--- src/routes/[[preview=preview]]/+page.svelte
+++ src/routes/[[preview=preview]]/+page.svelte
@@ -14,24 +14,27 @@
 
 
 <svelte:window bind:innerWidth={viewpoortWidth} />
-	<PrismicImage field={content.s1_image} class="w-full h-full top-0 right-1/12 fixed opacity-40" />
+	<PrismicImage field={content.s1_image} class="w-full top-1/2 -translate-y-1/2 right-1/12 fixed opacity-40" />
 
-<section id="top" class="w-screen h-lvh from-white/20 to-light/90 bg-linear-to-br py-32 relative add-noise overflow-y-visible">
-	<ContentWidth class="h-full overflow-y-visible">
-		<div class="w-1/2 h-full flex flex-col pt-32 pr-16">
+<section id="top" class="w-screen h-full min-h-lvh from-white/20 to-light/90 bg-linear-to-br py-32 relative add-noise overflow-y-visible">
+	<ContentWidth class="h-full">
+		<div class="md:w-1/2 flex flex-col md:pt-32 md:pr-16">
 			<h3 class="text-primary">{content.s1_eyebrow}</h3>
 			<h1 class="text-black">{content.s1_title}</h1>
 			<p class="mt-4">{content.s1_dates}</p>
 		</div>
+		<PrismicImage field={content.s1_image} class="md:hidden mt-6" />
 	</ContentWidth>
-	<PrismicImage field={content.s1_image} class="w-1/2 top-1/2 -translate-y-1/2 right-0 absolute" />
+	<PrismicImage field={content.s1_image} class="hidden md:w-1/2 top-1/2 md:-translate-y-1/2 right-0 md:absolute" />
 
 
 </section>
-<section id="qr" class="w-screen from-white/20 to-light/90 bg-linear-to-tr pb-16 relative add-noise flex flex-col items-center justify-center gap-12">
+<section id="qr" class="w-screen from-white/20 to-light/90 bg-linear-to-tr pb-16 relative add-noise ">
+	<ContentWidth class="flex flex-col items-center justify-center gap-12">
 	<h2 class="text-primary">{content.s2_rsvp_label}</h2>
-	<PrismicLink field={content.s2_qr_link} class="flex flex-col items-center">
-		<PrismicImage field={content.s2_qr} class="aspect-square w-full max-w-[66lvh]" />
-		<div>click here</div>
-	</PrismicLink>
+		<PrismicLink field={content.s2_qr_link} class="flex flex-col items-center">
+			<PrismicImage field={content.s2_qr} class="aspect-square w-full max-w-[66lvh]" />
+			<div>click here</div>
+		</PrismicLink>
+	</ContentWidth>
 </section>
